ea2de3c68c8d4eafa998ed54ec9aacd65f5669b8
Robert Gordon <Komeuppance@gmail.com>
2019-11-13 12:24:06 -0800

    App.js complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import Team from './components/Team';
+import Form from './components/Form'
+
 import './App.css';
 
 function App() {
+
+  const [team, setTeam] = useState([
+    {
+      id: 1,
+      name: "Robert Gordon",
+      email: "notmyrealemail@fakemails.com",
+      role: "Captain Obvious"
+    }
+  ]);
+
+  const addTeam = team => {
+    const newTeam = {
+      id: Date.now(),
+      name: team.name,
+      email: team.email,
+      role: team.role
+    };
+    setTeam([...team, newTeam]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Go Team!</h1>
       </header>
+    <div className='team-cards'>
+      <Form addTeam={addTeam}/>
+      <Team team={team} />
+    </div>
     </div>
   );
 }
