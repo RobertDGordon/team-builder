7be958e3b21a8ed7d4e5c860dabfb9dd372409c8
Robert Gordon <Komeuppance@gmail.com>
2019-11-14 00:46:27 -0800

    Edit button halfway working

--- src/App.js
+++ src/App.js
@@ -13,14 +13,24 @@ function App() {
       name: "Robert Gordon",
       email: "notmyrealemail@fakemails.com",
       role: "Captain Obvious",
-      imgurl: "https://ca.slack-edge.com/T4JUEB3ME-UN66JA72N-44abe8ad8258-512"
+      imgurl: "https://ca.slack-edge.com/T4JUEB3ME-UN66JA72N-44abe8ad8258-512",
+      desc: "All work and no play makes Robert a dull boy, all work and no play makes Robert a dull boy, all work and no play makes Robert a dull boy..."
     },
     {
       id: 2,
       name: "Captain Planet",
       email: "powerscombined@zeropollution.org",
       role: "Planeteer",
-      imgurl: "https://captainplanetfoundation.org/wp-content/uploads/CaptainPlanet.png"
+      imgurl: "https://captainplanetfoundation.org/wp-content/uploads/CaptainPlanet.png",
+      desc: "He's our hero. Gonna take pollution down to zero. Gonna help him put asunder, Bad guys who like to loot and plunder."
+    },
+    {
+      id: 3,
+      name: "Rick Astley",
+      email: "rick@roll.com",
+      role: "No Stranger To Love",
+      imgurl: "https://jeffdeviller.files.wordpress.com/2011/04/14-rickroll.jpg",
+      desc: "He's never going to give you up, never gonna let you down, never gonna run around a desert you, never gonna make you cry, never gonna say goodbye, never gonna tell a lie and hurt you."
     }
   ]);
 
@@ -30,23 +40,31 @@ function App() {
       name: team.name,
       email: team.email,
       role: team.role,
-      imgurl: team.imgurl
+      imgurl: team.imgurl,
+      desc: team.desc
     };
     setTeams([...teams, newTeam]);
   };
 
-  const memberToEdit = team=> {
-
-  };
+  const editTeam = newTeam =>{
+    let teamCopy = [...teams];
+    for (let i = 0; i < teamCopy.length; i++){
+      if (teamCopy[i].id === newTeam.id) {
+        teamCopy.splice(i, 1, newTeam)
+      }
+    }
+    setTeams(teamCopy);
+  }
 
   return (
     <div className="App">
-        <h1>Go Team!</h1>
+        <h1>Join Team Robert Today!
+        </h1>
       <div className='form-card'>
         <Form addNewTeam={addNewTeam}/>
       </div>
       <div className='team-cards'>
-        <Team teams={teams} memberToEdit={memberToEdit}/>
+        <Team teams={teams} editTeam={editTeam}/>
       </div>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 
 const Form = props => {
+
     const [team, setTeam] = useState({
         name: "",
         role: "",
         email: "",
-        imgurl: ""
+        imgurl: "",
+        desc: ""
     });
+
     const handleChanges = e => {
         setTeam({...team, [e.target.name]: e.target.value});
         // console.log (e.target.value);
@@ -14,8 +17,8 @@ const Form = props => {
     const submitForm = e => {
         e.preventDefault();
         props.addNewTeam(team);
-        console.log (team)
-        setTeam({name:"", role:"", email:"", imgurl:""})
+        // console.log (team)
+        setTeam({name:"", role:"", email:"", imgurl:"", desc:""})
     }
 
     return (
@@ -27,8 +30,10 @@ const Form = props => {
             <input id="role" type="text" name="role" onChange={handleChanges} value={team.role} />
             <label htmlFor="email">Email:</label>
             <input id="email" type="text" name="email" onChange={handleChanges} value={team.email} />
-            <label htmlFor="imgurl">Image URL:</label>
+            <label htmlFor="imgurl">Image:</label>
             <input id="imgurl" type="text" name="imgurl" onChange={handleChanges} value={team.imgurl} />
+            <label htmlFor="desc">Description:</label>
+            <input id="desc" type="text" name="desc" onChange={handleChanges} value={team.desc} />
             <button type="submit">Add</button>
         </form>
         </div>
